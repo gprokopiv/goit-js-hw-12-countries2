cccdbc6fc1a4cc7fb5553699e19e13480157f8c0
Prokopiv <halyna.kotlyar@gmail.com>
2021-01-22 10:59:43 +0200

    countries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles.css';
+import './js/fetchCountries';
+import './js/markup';
+import './js/pnotify';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css'; 
